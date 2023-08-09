6f763b69ccd4180473209ba1375fbda8be635288
kaung htet naing <11kaungkaung99@gmail.com>
2023-08-09 23:13:35 +0700

    setup google-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Footer, Navbar } from "@components/index";
 import "./globals.css";
 import { roboto_slab } from "@utils/fonts";
 import ResumeProvider from "@context/resumeProvider";
+import Script from "next/script";
 
 export const metadata = {
 	title: "I'm Kaung Htet Naing,Front-end Developer",
@@ -34,6 +35,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<Script strategy="lazyOnload" id="google-analytics">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+        `}
+			</Script>
 			<body suppressHydrationWarning className={roboto_slab.className}>
 				<ResumeProvider>
 					<Navbar>{children}</Navbar>
